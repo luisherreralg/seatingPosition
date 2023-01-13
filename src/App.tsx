8dd2fbca15bbf48f47c19dff24cab3794eafb11c
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2023-01-13 17:08:10 +0100

    ✨ Random changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Random from "./utils/Random";
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { table } from "console";
 
 function App() {
+  const studentsInitialState = Random();
+  const teachers = [
+    {
+      name: "Profe1",
+      img: "https://planb.mx/wp-content/uploads/2022/04/FB_IMG_1621909877113-1.jpg",
+    },
+    {
+      name: "Profe2",
+      img: "https://www.laguiadelvaron.com/wp-content/uploads/2019/08/historia-del-meme-gato-en-mesa-www.laguiadelvaron-3.jpg",
+    },
+    {
+      name: "Alejandro",
+      img: "https://media.istockphoto.com/id/502295596/photo/funny-awkward-moose-eating.jpg?s=612x612&w=0&k=20&c=3iklofsMm6z6vs7a0b_yJDd0gGMOnhb84tWwn126ktw=",
+    },
+    {
+      name: "Profe3",
+      img: "https://i.kym-cdn.com/photos/images/facebook/001/309/269/10a.png",
+    },
+  ];
+
+  const [students, setStudents] = useState(studentsInitialState);
+
+  const reorder = (list: Array<any>, startIndex: number, endIndex: number) => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext
+      onDragEnd={(result) => {
+        const { source, destination } = result;
+        if (
+          !destination ||
+          (source.index === destination.index &&
+            source.droppableId === destination.droppableId)
+        )
+          return;
+        setStudents((prevStudent) =>
+          reorder(prevStudent, source.index, destination.index)
+        );
+      }}
+    >
+      <div className="App-container">
+        <Droppable droppableId="students-left">
+          {(droppableProvided) => (
+            <ul
+              {...droppableProvided.droppableProps}
+              ref={droppableProvided.innerRef}
+              className="App"
+            >
+              {students.map((student, index) => {
+                return (
+                  <Draggable
+                    key={student.name}
+                    draggableId={student.name}
+                    index={index}
+                  >
+                    {(draggableProvided) => (
+                      <li
+                        {...draggableProvided.draggableProps}
+                        ref={draggableProvided.innerRef}
+                        {...draggableProvided.dragHandleProps}
+                        className={`student student-${index + 1}`}
+                      >
+                        <p>{`${student.name}`}</p>
+                        <img src={student.img} alt={`${student.name}`} />
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+
+              {teachers.map((teacher, index) => {
+                return (
+                  <li
+                    key={teacher.name}
+                    className={`teacher teacher-${index + 1}`}
+                  >
+                    <p>{`${teacher.name}`}</p>
+                    <img src={teacher.img} alt={`${teacher.name}`} />
+                  </li>
+                );
+              })}
+
+              {new Array(10).fill(0).map((_status, index) => {
+                return (
+                  <li key={index} className={`table table-${index + 1}`} />
+                );
+              })}
+              {droppableProvided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
---
+++ src/utils/Random.tsx
@@ -0,0 +1,91 @@
+export default function Random() {
+  const students = [
+    {
+      name: "Antonio Jesús Ruiz",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U043A0XAJ4Q-dea68d6f29a4-512",
+    },
+    {
+      name: "Carlos Ruiz",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U044854PPLK-fd6ad1a7e82e-512",
+    },
+    {
+      name: "Ariadna Frias",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U045V7YMD7F-b1768b11ca1e-512",
+    },
+    {
+      name: "Covadonga Millan",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U0468NU0SQZ-44c42d04428c-512",
+    },
+    {
+      name: "Leandro Lupano",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U0475D6KZV0-9c24c871a42a-512",
+    },
+    {
+      name: "Rocío Ricciardiello",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U02NYSWV928-09d453fc9e58-512",
+    },
+    {
+      name: "Joaquin Bezanilla",
+      img: "https://a.pinatafarm.com/312x296/ae7f8ccd22/sad-thumbs-up-cat.jpg/m/522x0",
+    },
+    {
+      name: "Fabio Di Noia",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04892VF3K7-4249aa3f0d30-512",
+    },
+    {
+      name: "Daniel Ursúa",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U048NH40602-3e8cde9ef8eb-512",
+    },
+    {
+      name: "Jimena Nuñez",
+      img: "https://www.idlememe.com/wp-content/uploads/2021/10/beluga-cat-meme-idlememe-5-300x169.jpg",
+    },
+    {
+      name: "Fernando Tarraga",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U048WNHK3JB-1552d7b39531-512",
+    },
+    {
+      name: "Luis Periañez",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U049Z1UBH0F-d25af0c83bfe-512",
+    },
+    {
+      name: "Ryan Leary",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04A51Q7HFV-1996b5aed933-512",
+    },
+    {
+      name: "Gonzalo Díez",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04A38YQBAR-4e3fc8172750-512",
+    },
+    {
+      name: "Jesus Enrique Jimenez",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04AAKBF8JH-c4f2ab45561f-512",
+    },
+    {
+      name: "Silvia Oliva",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04A6NX6A6A-a263ed4e9013-512",
+    },
+    {
+      name: "Elena Bragado",
+      img: "https://i.ytimg.com/vi/Zr-qM5Vrd0g/maxresdefault.jpg",
+    },
+    {
+      name: "Ivan Dúran",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04ALQ7DJUS-99ca420aeb75-512",
+    },
+    {
+      name: "Mariano Quiroga",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U03RN0LLEBT-d2301df19d2a-512",
+    },
+    {
+      name: "Joseba Ibarlucea",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04B7GXFES2-f345dbb85fb4-512",
+    },
+    {
+      name: "Santiago Fernandez",
+      img: "https://ca.slack-edge.com/T0SJKHBFZ-U04CSBWBWL9-f47a5fdcb35f-512",
+    },
+  ];
+
+  const randomStudents = students.sort(() => Math.random() - 0.5);
+  return randomStudents;
+}
