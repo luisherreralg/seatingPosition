8f33c4e3667d7eaa51157cad0ae6e51b4d59c157
luisherreralg <lherreralg@outlook.es>
2023-02-10 00:06:27 +0100

    Added a new constrain

--- src/utils/SeatsConstrains.ts
+++ src/utils/SeatsConstrains.ts
@@ -1,10 +1,10 @@
-import { user } from "../Types/user";
+import { Student } from "../Types/models";
 
-const randomPosition = () => {
+const nearProjectorRange = () => {
   return Math.floor(Math.random() * 3);
 };
 
-export const danielConstrain = (randomStudents: user[]) => {
+export const danielConstrain = (randomStudents: Student[]) => {
   // ! Que Daniel ursula esté entre la posicion 0 y 3 o  10 y 13
   // (Tres primeras posiciones de las dos columenas pegadas al proyector)
   const danielIndex = randomStudents.findIndex(
@@ -13,16 +13,16 @@ export const danielConstrain = (randomStudents: user[]) => {
 
   if (danielIndex > 3 && danielIndex < 10) {
     const daniel = randomStudents.splice(danielIndex, 1);
-    randomStudents.splice(0 + randomPosition(), 0, daniel[0]);
+    randomStudents.splice(0 + nearProjectorRange(), 0, daniel[0]);
   } else if (danielIndex > 10) {
     const daniel = randomStudents.splice(danielIndex, 1);
-    randomStudents.splice(10 + randomPosition(), 0, daniel[0]);
+    randomStudents.splice(10 + nearProjectorRange(), 0, daniel[0]);
   }
 
   return randomStudents;
 };
 
-export const ryanGonzuConstrain = (randomStudents: user[]) => {
+export const ryanGonzuConstrain = (randomStudents: Student[]) => {
   // ! Evitar que Ryan Leary esté junto a Gonzalo Díez
   const ryanIndex = randomStudents.findIndex(
     (student) => student.name === "Ryan Leary"
@@ -39,7 +39,7 @@ export const ryanGonzuConstrain = (randomStudents: user[]) => {
   return randomStudents;
 };
 
-export const covaConstrain = (randomStudents: user[]) => {
+export const covaConstrain = (randomStudents: Student[]) => {
   // ! Que Daniel ursula esté entre la posicion 0 y 3 o  10 y 13
   // (Tres primeras posiciones de las dos columenas pegadas al proyector)
   const covaIndex = randomStudents.findIndex(
@@ -48,10 +48,27 @@ export const covaConstrain = (randomStudents: user[]) => {
 
   if (covaIndex > 3 && covaIndex < 10) {
     const cova = randomStudents.splice(covaIndex, 1);
-    randomStudents.splice(0 + randomPosition(), 0, cova[0]);
+    randomStudents.splice(0 + nearProjectorRange(), 0, cova[0]);
   } else if (covaIndex > 10) {
     const cova = randomStudents.splice(covaIndex, 1);
-    randomStudents.splice(10 + randomPosition(), 0, cova[0]);
+    randomStudents.splice(10 + nearProjectorRange(), 0, cova[0]);
+  }
+
+  return randomStudents;
+};
+
+export const JimenaGonzuConstrain = (randomStudents: Student[]) => {
+  // ! Evitar que Jimena esté junto a Gonzalo Díez
+  const jimeIndex = randomStudents.findIndex(
+    (student) => student.name === "Jimena González"
+  );
+  const gonzaloIndex = randomStudents.findIndex(
+    (student) => student.name === "Gonzalo Díez"
+  );
+
+  if (jimeIndex === gonzaloIndex - 1 || jimeIndex === gonzaloIndex + 1) {
+    const jime = randomStudents.splice(jimeIndex, 1);
+    randomStudents.splice(jimeIndex + Math.random() * 12, 0, jime[0]);
   }
 
   return randomStudents;
